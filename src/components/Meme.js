5948c06936b5f258a6768897afb6ce78b860e86b
Sandesh GC <gcsandesh01@gmail.com>
2022-08-11 00:55:55 +0545

    added text to memes using inputs

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,34 +1,70 @@
 import React from "react";
 // import memeImage from "../images/meme.png";
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
- console.log(memeImage)
-//  console.log(setMemeImage)
-  function getMemeImage(){
+  // const [memeImage, setMemeImage] = React.useState(""); //state that is init with memeImage
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    memeImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  console.log(meme);
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+  //  console.log(setMemeImage)
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(meme.topText);
+    console.log(meme.bottomText);
+    console.log(meme.memeImage);
+    // setMeme((prevMeme) => ({ ...prevMeme, memeImage: { randomImage } }));
+    //function to get meme image
     //change the link later
-    setMemeImage("https://i.imgflip.com/43a45p.png");
+    // setMemeImage("http://i.imgflip.com/1bij.jpg");
   }
 
   return (
-    <div className="meme--container max-w-5xl ml-auto mr-auto w-full">
-      <div className="w-1/2 ml-auto mr-auto flex flex-col">
+    <div className="meme--container w-fit mt-12 max-w-5xl ml-auto mr-auto">
+      <form onSubmit={handleSubmit} className="flex flex-col">
         <div className="flex justify-between items-center">
           <input
-            name="meme__text--top"
+            name="topText"
+            value={meme.topText}
             placeholder="Top text"
+            onChange={handleChange}
             className="rounded-md w-1/2  max-w-sm mr-2 h-10 border-2 pl-2 pr-2 text-sm mt-4"
           />
           <input
-            name="meme__text--bottom"
+            name="bottomText"
+            value={meme.bottomText}
             placeholder="Bottom text"
+            onChange={handleChange}
             className=" rounded-md w-1/2 max-w-sm ml-2 h-10 border-2 pl-2 pr-2 text-sm mt-4"
           />
         </div>
-        <button onClick={getMemeImage} className="rounded-md primary-gradient text-white h-10 mt-4">
+        <button className="rounded-md primary-gradient text-white h-10 mt-4">
           Get a new meme image 🖼
         </button>
+      </form>
+      <div className="meme relative right-0 mt-6">
+        <img
+          src={meme.memeImage}
+          alt="a meme"
+          className="meme__img w-full ml-auto mr-auto rounded-md"
+        />
+        <h1 className="meme__text uppercase text-5xl text-center w-full m-0 text-white top-1 absolute tracking-tighter font-extrabold">
+          {meme.topText}
+        </h1>
+        <h1 className="meme__text uppercase text-5xl text-center w-full m-0 text-white bottom-1 absolute tracking-tighter font-extrabold">
+          {meme.bottomText}
+        </h1>
       </div>
-      <img src={memeImage} alt="a meme" className="meme__img w-1/2 ml-auto mr-auto mt-6" />
     </div>
   );
 }
